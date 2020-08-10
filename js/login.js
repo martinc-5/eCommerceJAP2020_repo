b87694dc93acaa14025afdc498404b473ac21e89
Martin Cabillon <mcabillon5@gmail.com>
2020-08-10 18:31:18 -0300

    se agregó validacion de login vía login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function validacion() {
 
-});
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+
+if( username == null || username.length == 0 || /^\s+$/.test(username) ) {
+      // Si no se cumple la condicion...
+      alert('[ERROR] El campo de nombre de usuario no puede estar vacío');
+      return false;
+    }
+    else if (password == null || password.length == 0 || /^\s+$/.test(password) ) {
+        // Si no se cumple la condicion...
+        alert('[ERROR] El campo de contraseña no puede estar vacío');
+        return false;
+      }
+    
+  
+    // Si el script ha llegado a este punto, todas las condiciones
+    // se han cumplido, por lo que se devuelve el valor true
+    return true;
+
+  }
+
+  
+
+  
